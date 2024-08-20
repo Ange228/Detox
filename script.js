49697d04c5366d2417dfca6ec164e4b9dd1081b9
Ange228 <acastilloto@ulasalle.edu.pe>
2024-08-20 09:11:22 -0500

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Meta Pixel Code
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '8034086150021614');
+fbq('track', 'PageView');
+// Fin del código Meta Pixel
+
+// Resto de tu código JavaScript
+
 // Índice inicial para el carrusel
 let currentIndex = 0;
 
