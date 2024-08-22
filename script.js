cbef4fb379b76ce370a60666265bfe7225a5cca3
Ange228 <acastilloto@ulasalle.edu.pe>
2024-08-22 08:31:15 -0500

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '8034086150021614');
+fbq('init', '1532960383914242');
 fbq('track', 'PageView');
 // Fin del código Meta Pixel
 
