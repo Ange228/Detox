4393244788f8d016d6c1bc8c6a82eeca629d9abc
Diego Paolo Nova Rosas <dnovar@ulasalle.edu.pe>
2024-09-20 05:36:43 -0500

    Corrigiendo Pixel, estaba con comentarios en HTML en el código JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-<!-- Meta Pixel Code -->
-<script>
+
+//Pixel
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -11,11 +11,7 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '881446550606263');
 fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=881446550606263&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
+//Pixel
 
 // Resto de js
 
